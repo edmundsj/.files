d2efbace353c578258892c96f3ad66320a4ba897
Jordan <edmundsj@uci.edu>
2018-04-20 06:24:39 -0700

    Added pragmatic programmer notes, rest notes, and updated git/book notes

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 git config --global user.email edmundsj@uci.edu
 git config --global user.name "Jordan"
+
+git clone https://github.com/edmundsj/.files .files
+
+# Install Sphinx using pip install Sphinx for python documentation
