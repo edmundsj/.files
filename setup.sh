5b6820a3df488224784440a715cfa65b8626cdca
Jordan <edmundsj@uci.edu>
2018-10-24 10:24:52 -0700

    updated setup file to fix vim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ git config --global user.name "Jordan"
 
 git clone https://github.com/edmundsj/.files .files
 
+cp .files/.vimrc ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Install Sphinx using pip install Sphinx for python documentation
 
 # Should next install bundle, and all the associated plugins.
