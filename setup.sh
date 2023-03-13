6de536050b50c30a0f06c6efbffc07902702674b
Jordan <edmundsj@uci.edu>
2023-03-13 11:00:49 -0400

    added pip installation, default packages to setup.sh, added bashrc tracking and copying

--- setup.sh
+++ setup.sh
@@ -1,12 +1,65 @@
+# Install gh CLI
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# Authenticate to github
+gh auth login
+
+# Setup git shortcuts
 git config --global user.email edmundsj@uci.edu
 git config --global user.name "Jordan"
+git config --global alias.rb rebase
+git config --global alias.l log
+git config --global alias.c commit
+git config --global alias.a add
 
-git clone https://github.com/edmundsj/.files .files
+cp .vimrc ~/
+cp .bashrc ~/
 
-cp .files/.vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt update
+sudo apt install -y terminator
+sudo apt install -y python3-pip
+pip insatll -r requirements.txt
 
-# Install Sphinx using pip install Sphinx for python documentation
+sudo ufw enable
+sudo ufw allow ssh
 
-# Should next install bundle, and all the associated plugins.
+# Install miscellaneous developer tools
+sudo apt install -y make s3cmd postgresql-client-common postgresql-client
+sudo snap install doctl
 
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install docker and docker dependencies
+sudo apt install -y curl gnupg lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install latest LTS nvm and node
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install --lts
+nvm use --lts
+
+# Install terraform and terraform dependencies
+sudo apt install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform
